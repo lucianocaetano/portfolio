8d69158e2c987534fc3e0079867f28f35513d7ed
Sara <sarag4063@gmail.com>
2025-04-01 18:38:29 -0300

    Changes to the contacts page (a form and company details have been added)

---
+++ src/app/(portfolio)/contact/components/ui/focus-cards.tsx
@@ -0,0 +1,107 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/(portfolio)/contact/components/ui/dialog"
+
+type Card = {
+  title: string;
+  content: string;
+  src: string;
+};
+
+export const Card = React.memo(
+  ({
+    card,
+    index,
+    hovered,
+    setHovered,
+  }: {
+    card: Card;
+    index: number;
+    hovered: number | null;
+    setHovered: React.Dispatch<React.SetStateAction<number | null>>;
+  }) => (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div
+          onMouseEnter={() => setHovered(index)}
+          onMouseLeave={() => setHovered(null)}
+          className={cn(
+            "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out cursor-pointer",
+            hovered !== null && hovered !== index && "blur-sm scale-[0.98]"
+          )}
+        >
+          <Image
+            src={card.src}
+            alt={card.title}
+            fill
+            className="object-cover absolute inset-0"
+          />
+          <div
+            className={cn(
+              "absolute inset-0 bg-black/50 flex items-end py-8 px-4 transition-opacity duration-300",
+              hovered === index ? "opacity-100" : "opacity-0"
+            )}
+          >
+            <div className="text-xl md:text-2xl font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200">
+              {card.title}
+            </div>
+          </div>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="h-[90%]">
+        <div
+          className={cn(
+            "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-full w-full",
+          )}
+        >
+          <Image
+            src={card.src}
+            alt={card.title}
+            fill
+            className="object-cover absolute inset-0"
+          />
+          <div
+            className="absolute inset-0 bg-black/50 flex items-end py-8 px-4 transition-opacity duration-300" 
+          >
+            <DialogHeader className="text-xl md:text-2xl font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200 gap-6">
+              <DialogTitle className="text-3xl">{card.title}</DialogTitle>
+              <DialogDescription className="text-white">
+                {card.content}    
+              </DialogDescription>
+            </DialogHeader>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+);
+
+Card.displayName = "Card";
+
+export function FocusCards({ cards }: { cards: Card[] }) {
+  const [hovered, setHovered] = useState<number | null>(null);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto md:px-8 w-full">
+      {cards.map((card, index) => (
+        <Card
+          key={card.title}
+          card={card}
+          index={index}
+          hovered={hovered}
+          setHovered={setHovered}
+        />
+      ))}
+    </div>
+  );
+}
