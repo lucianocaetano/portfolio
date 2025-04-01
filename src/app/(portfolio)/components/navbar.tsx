d16fca7e4f5b97de245615116363fa6ce46f927f
Mauro Caetano <lucianocaetano1999@gmail.com>
2025-04-01 18:53:56 -0300

    navbar de santiago ahora si

--- src/app/(portfolio)/components/navbar.tsx
+++ src/app/(portfolio)/components/navbar.tsx
@@ -12,7 +12,7 @@ import Link from "next/link"
 const Navbar = () => {
 
   return (
-    <nav className="fixed z-20 w-full bg-gray-100 dark:bg-[#26262680] backdrop-blur-md"> 
+    <nav className="fixed z-20 w-full backdrop-blur-md"> 
       <div className="w-full max-w-7xl mx-auto flex justify-between items-center h-16 px-4">
         <div>
           <NavigationMenu>
